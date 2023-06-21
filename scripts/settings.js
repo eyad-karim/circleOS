08e6822a47ac2b7bc65e7e85a3fda723260988c5
Eyad Karim <eyad.karim.r3@gmail.com>
2023-06-20 22:06:17 -0700

    fix error

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,4 @@
 function changeBG(bgName) {
-    main.style.backgroundImage = `url(../media/${bgName})`;
-    document.getElementById("current-background").style.backgroundImage = `url(../media/${bgName})`;
+    main.style.backgroundImage = `url('./media/${bgName}')`;
+    document.getElementById("current-background").style.backgroundImage = `url('./media/${bgName}')`;
 }
