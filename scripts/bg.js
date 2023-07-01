c1c2f3eb54ab2e8dc3876b74d865f959ce2be2e3
Eyad Karim <eyad.karim.r3@gmail.com>
2023-07-01 15:40:49 -0700

    change themes

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,6 +1,6 @@
 function changeBG(bgName) {
-    main.style.backgroundImage = `url('./media/${bgName}')`;
-    document.getElementById("current-background").style.backgroundImage = `url('./media/${bgName}')`;
+    main.style.backgroundImage = `url('./media/bgs/${bgName}')`;
+    document.getElementById("current-background").style.backgroundImage = `url('./media/bgs/${bgName}')`;
 }
 function uploadBG() {
     main.style.backgroundImage = `url('${URL.createObjectURL(customBgBtn.files[0])}')`;
